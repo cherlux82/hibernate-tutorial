4e0ba9e2c97cbd62948a312c2075753c0a80e59a
cherlux82 <icc.vicente.garcia@hotmail.com>
2022-02-09 22:53:03 -0600

    fin de seccion 27. Inicia video 264

---
+++ src/com/draggerco/hibernate/mapping/manytomany/DeleteCoursesForStudentsDemo.java
@@ -0,0 +1,47 @@
+package com.draggerco.hibernate.mapping.manytomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.draggerco.hibernate.mapping.manytomany.entity.Course;
+import com.draggerco.hibernate.mapping.manytomany.entity.Instructor;
+import com.draggerco.hibernate.mapping.manytomany.entity.InstructorDetail;
+import com.draggerco.hibernate.mapping.manytomany.entity.Review;
+import com.draggerco.hibernate.mapping.manytomany.entity.Student;
+
+public class DeleteCoursesForStudentsDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.ManyToMany.xml")
+				.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class).addAnnotatedClass(Review.class).addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			System.out.println("\n=====================================\n");
+			session.beginTransaction();
+			System.out.println("\n=====================================\n");
+			int id = 11;
+
+			Course s = session.get(Course.class, id);
+			System.out.println(s);
+
+			System.out.println("\n=====================================\n");
+
+			session.delete(s);
+			session.getTransaction().commit();
+			System.out.println("\n=====================================\n");
+
+			System.out.println("Done! ...");
+			System.out.println("\n=====================================\n");
+
+		} finally {
+			factory.close();
+		}
+	}
+
+}
